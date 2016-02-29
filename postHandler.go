71dae6e56326b4d5b54acfc294efc9bf1b3781f4
Ben Brown <ben.p.brown@gmail.com>
2016-02-29 17:22:34 -0500

    First work on get all messages

--- postHandler.go
+++ postHandler.go
@@ -7,27 +7,55 @@ import (
 )
 
 func postComment(w http.ResponseWriter, req *http.Request) {
-	type successOrFail struct {
+	type responseStruct struct {
 		Ok       bool
 		ErrorMsg string
 	}
 	if req.Method != "POST" {
 		// User trying to GET the page
-		p := successOrFail{false, "Can only POST this endpoint"}
+		p := responseStruct{false, "Can only POST this endpoint"}
 		json.NewEncoder(w).Encode(p)
 		return
 	}
 	req.ParseForm()
 	var userMessage = req.Form.Get("message")
-	_, err := db.Exec("INSERT INTO messages(id, msg) VALUES (?, ?)", nil, userMessage)
-
-	if err != nil {
-		log.Print(err)
-		p := successOrFail{false, "DB error"}
+	if userMessage == "" {
+		p := responseStruct{false, "Message may not be empty"}
 		json.NewEncoder(w).Encode(p)
 		return
 	}
 
-	p := successOrFail{true, ""}
+	_, err := db.Exec("INSERT INTO messages(id, msg) VALUES (?, ?)", nil, userMessage)
+
+	if err != nil {
+		log.Print(err)
+		p := responseStruct{false, "DB error"}
+		json.NewEncoder(w).Encode(p)
+		return
+	}
+
+	p := responseStruct{true, ""}
 	json.NewEncoder(w).Encode(p)
+}
+
+func getAllMessages(w http.ResponseWriter, req *http.Request) {
+	type resultStruct struct {
+		Ok       bool
+		ErrorMsg string
+		Messages []string
+	}
+	rows, err := db.Query("SELECT * FROM messages")
+	defer rows.Close()
+	if err != nil {
+		log.Print(err)
+		p := resultStruct{false, "DB error", make([]string, 0, 0)}
+		json.NewEncoder(w).Encode(p)
+		return
+	}
+	for rows.Next() {
+		var id int
+		var message string
+		err = rows.Scan(&id, &message)
+	}
+
 }
