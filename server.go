c391c26fd8400248d429601742463b1155647a5d
maxbittker <maxbittker@gmail.com>
2016-02-29 16:22:16 -0500

    serves basics

--- server.go
+++ server.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "html"
+  "log"
+)
 
 func main() {
 	fmt.Println("Hello, 世界")
+
+  handleRequest := func(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "<h1>%q</h1>", html.EscapeString(r.URL.Path))
+  }
+
+  http.HandleFunc("/test", handleRequest)
+
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
