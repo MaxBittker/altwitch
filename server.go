8994f054b8cfb0d0c1a96c86acc1078cf615dbc3
Ben Brown <ben.p.brown@gmail.com>
2016-02-29 16:45:28 -0500

    Connect to in-memory sqlite

--- server.go
+++ server.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "html"
-  "log"
+	"database/sql"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"github.com/mattn/go-sqlite3"
 )
 
+
+
 func main() {
-	fmt.Println("Hello, 世界")
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		trueErr, ok := err.(sqlite3.Error)
+		if !ok {
+			log.Fatal(err)
+		}
+		log.Fatal(trueErr)
+	}
+	defer db.Close()
+	db.Ping()
 
-  handleRequest := func(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "<h1>%q</h1>", html.EscapeString(r.URL.Path))
-  }
 
-  http.HandleFunc("/test", handleRequest)
+	handleRequest := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>%q</h1>", html.EscapeString(r.URL.Path))
+	}
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/test", handleRequest)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
