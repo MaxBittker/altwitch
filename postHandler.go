82f8149bdd177495cd164b18689e496588d610ec
Ben Brown <ben.p.brown@gmail.com>
2016-02-29 17:01:30 -0500

    post method for new messages

--- postHandler.go
+++ postHandler.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"net/http"
+	"log"
+	"encoding/json"
 )
 
 func postComment(w http.ResponseWriter, req *http.Request) {
+	type successOrFail struct {
+		ok bool
+		errormsg string
+	}
 	if req.Method != "POST" {
 		// User trying to GET the page
-		http.Error(w, "Only POST", 400);
+		p := successOrFail{false, "Can only POST this endpoint"}
+		json.NewEncoder(w).Encode(p)
 		return
 	}
 	req.ParseForm()
 	var userMessage = req.Form.Get("message")
-	res, err := db.Exec("INSERT INTO messages(id, url) VALUES (?, ?)", nil, userMessage)
+	_, err := db.Exec("INSERT INTO messages(id, url) VALUES (?, ?)", nil, userMessage)
 	if err != nil {
-		
+		log.Fatal(err)
+		p := successOrFail{false, "DB error"}
+		json.NewEncoder(w).Encode(p)
 	}
+	
+	p := successOrFail{true, ""}
+	json.NewEncoder(w).Encode(p)
 }
