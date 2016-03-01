1701809cbfe5586198d98666d89ab0231fe97157
Ben Brown <ben.p.brown@gmail.com>
2016-03-01 18:49:22 -0500

    Comments for database instantiation

--- server.go
+++ server.go
@@ -13,16 +13,22 @@ func main() {
 	var err error
 	db, err = sql.Open("sqlite3", ":memory:")
 	if err != nil {
+		// Cast the error interface to a sqlite error
+		// May allow us to get some more information
 		trueErr, ok := err.(sqlite3.Error)
+		// Check if cast failed
 		if !ok {
 			log.Fatal(err)
 		}
 		log.Fatal(trueErr)
 	}
+	// Remember to close the database
 	defer db.Close()
+	// Actually make a connection, will write the database to disk if necessary
 	db.Ping()
 
 	sqlStmt := "CREATE TABLE IF NOT EXISTS messages ( id INTEGER NOT NULL PRIMARY KEY, sender TEXT NOT NULL, msg TEXT NOT NULL);"
+	// We don't care about the result, only whether or not it failed
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		log.Fatal(err)
