0e2623bc67b02d06b2dd3537b1b5d50d45c16ae7
Ben Brown <ben.p.brown@gmail.com>
2016-03-01 20:48:15 -0500

    Comments for Giphy feature

--- websocketLobby.go
+++ websocketLobby.go
@@ -25,7 +25,8 @@ type wsClient struct {
 
 var firstId uint32 = 0
 var mutex = &sync.Mutex{}
-
+// This is used to, in a parallel-safe way, get a unique
+// monotonically increasing user id for each new socket connections
 func getNextId() *uint32 {
 	mutex.Lock()
 	returnInt := firstId
@@ -34,12 +35,18 @@ func getNextId() *uint32 {
 	return &returnInt
 }
 
+// This structure is used internally, because it exposes
+// our interal userId value, which is a unique
+// monotonically increasing key that is tied to a specific
+// socket connection
 type internalWebsocketMessageStruct struct {
 	Message []byte
 	Sender  []byte
 	UserId  *uint32
 }
 
+// This is the structure that is both received from the
+// clients and sent back to them. No userId is present.
 type externalWebsocketMessageStruct struct {
 	Message string
 	Sender  string
@@ -85,6 +92,13 @@ func (client *wsClient) readMessages() {
 	}
 }
 
+// This takes in a search term, a sender's name, and that sender's user id
+// and fetches the first Giphy result corresponding to that search term.
+// It then puts that result into the lobby for broadcast.
+
+// This function writes raw HTML, so care has to be taken
+// to ensure that the user's name and the user's search
+// term are not going to contain unsafe HTML characters
 func sendGif(searchTerm string, sender string, userId *uint32) {
 	searchTerm = url.QueryEscape(searchTerm)
 	co := &gophy.ClientOptions{}
@@ -101,6 +115,8 @@ func sendGif(searchTerm string, sender string, userId *uint32) {
 	}
 }
 
+// Writes all outstanding messages, in a loop,
+// to the connected client.
 func (client *wsClient) writeMessages() {
 	defer client.conn.Close()
 	defer log.Println("writeMessages exiting")
@@ -130,6 +146,7 @@ type lobby struct {
 	unregister chan *wsClient
 }
 
+// Actual instantiation of the lobby.
 var theLobby = lobby{
 	clients:    make(map[*wsClient]bool),
 	broadcast:  make(chan internalWebsocketMessageStruct),
@@ -137,8 +154,13 @@ var theLobby = lobby{
 	unregister: make(chan *wsClient),
 }
 
+// Main lobby function whose main purpose is to keep
+// track of the currently connected clients, and to
+// broadcast messages to them.
 func (l *lobby) serveLobby() {
 	for {
+		// "Zipper" threading - if you can, register a new client,
+		// if not, unregister, if not, broadcast a message
 		select {
 		case conn := <-l.register:
 			// Get the map set up for this input
@@ -157,6 +179,7 @@ func (l *lobby) serveLobby() {
 					theSender = []byte("You")
 				}
 				externalStruct := externalWebsocketMessageStruct{Message: string(msgStruct.Message), Sender: string(theSender)}
+				// This really shouldn't fail because it was created internally
 				marshalled, err := json.Marshal(externalStruct)
 				if err == nil {
 					select {
